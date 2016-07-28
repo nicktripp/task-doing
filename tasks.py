7eed66677ed6a8aa6d1f17c7650c8633dfda237d
Nick <nick.e.tripp@gmail.com>
2016-07-28 00:00:27 -0500

    add modularization and methods

--- tasks.py
+++ tasks.py
@@ -1,34 +1,50 @@
 #==============================================================================#
 # This file takes command line input to control the task manager.
-# 
+#
 # @author Nick Tripp
-# @date June 2016
+# @date July 2016
+#
+#TODO: prioritize tasks!
+#TODO: print color?
 #==============================================================================#
-
-
 import argparse
 import os
 
-parser = argparse.ArgumentParser(description='Manages task list.')
-group = parser.add_mutually_exclusive_group()
-group.add_argument('-a, --add', dest='new_task', default=None, help='Add a task to the task list')
-group.add_argument('-d, --delete', dest='del_task', default=None, help='Delete a task from the task list')
+def main():
+    parser = argparse.ArgumentParser(description='Manages task list.')
+    group = parser.add_mutually_exclusive_group()
+    group.add_argument('-a, --add', dest='new_task', default=None, help='Add a task to the task list')
+    group.add_argument('-d, --delete', dest='del_task', default=None, help='Delete a task from the task list')
+    group.add_argument('-c, --complete', dest='comp_task', default=None, help='Mark a task as completed')
 
-args = parser.parse_args()
+    args = parser.parse_args()
 
-if(args.new_task != None):
-    # ADD TASK TO LIST
+    if(args.new_task != None):
+        add_task(args.new_task)
+    elif(args.del_task != None):
+        remove_task(args.del_task)
+    elif(args.comp_task != None):
+        # STRIKETHROUGH TASK
+    else:
+        # PRINT TASK LIST: file
+        f = open(os.environ['TASKPATH'] + "tasks.txt")
+        print("\n== CURRENT TASKS ==\n")
+        for line in f:
+            print(" - " + line),
+        print("")
+
+def add_task(new_task):
     f = open(os.environ['TASKPATH'] + 'tasks.txt', 'a')
-    f.write(args.new_task + "\n");
-    print("Wrote new task: " + args.new_task)
-elif(args.del_task != None):
-    # REMOVE TASK FROM LIST
+    f.write(new_task + "\n")
+    print("Wrote new task: " + new_task)
+
+def remove_task(task):
     f = open(os.environ['TASKPATH'] + 'tasks.txt', 'r+')
     lines = f.readlines()
     f.seek(0)
     found = None
     for line in lines:
-        if(not found and line.lower().find(args.del_task.lower()) == 0):
+        if(not found and line.lower().find(task.lower()) == 0):
             found = line
         else:
             f.write(line)
@@ -39,15 +55,8 @@ elif(args.del_task != None):
     if(found):
         print("Deleted task: " + found),
     else:
-        print("Could not find task: " + args.del_task)
-else:
-    # PRINT TASK LIST: file
-    f = open(os.environ['TASKPATH'] + "tasks.txt")
-    print("\n== CURRENT TASKS ==\n")
-    for line in f:
-        print(" - " + line),
-    print("")
+        print("Could not find task: " + task)
 
-#TODO: prioritize tasks!
-#TODO: print color?
 
+
+if __name__ == '__main__': main()
