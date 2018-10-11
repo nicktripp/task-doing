dcd33747c6ae6ce567226b786d3b4ac677e4ac51
nicktripp <nick.e.tripp@gmail.com>
2018-10-11 00:44:12 -0700

    add 'tasks list' parser to logic connection

--- src/tasks.py
+++ src/tasks.py
@@ -12,6 +12,7 @@
 #TODO: Handle delete collisions by asking to clarify
 #TODO: Undo action button (via another JSON file!)
 #TODO: Switch from task lists to task sets, and from delete by index to delete by ID
+#TODO: multiple task add
 #==============================================================================#
 import argparse
 import os
@@ -39,17 +40,17 @@ def print_track_header(task_list, track, is_color=True):
         head_str = task_list.get_track_color(track) + head_str + color.END
     print(head_str.format(" {} ".format(track)))
 
-def print_header():
+def print_header(title):
     rows, columns = os.popen('stty size', 'r').read().split()
     head_str= color.BOLD + "{:=^" + columns + "}\n" + color.END
-    print(head_str.format(" TASKS "))
+    print(head_str.format(" {} ".format(title)))
 
 def main():
     data_file = os.path.join(get_data_path(), 'tasks.JSON')
     task_list = TaskList(data_file)
 
     parser = argparse.ArgumentParser(description='Manages task list.')
-    parser.add_argument('-v','--verbose', action='count', help='display more information')
+    parser.add_argument('-v','--verbose', action='count', help='display more information') # TODO: connect parser
 
     subparsers = parser.add_subparsers(required=True, dest='subcommand')
 
@@ -80,15 +81,15 @@ def main():
     parser_comp.add_argument('-n','--num', dest='comp_task_num', type=int, help='complete a task by its index within a track instead of by text')
     parser_comp.add_argument('-t','--track', dest='comp_track', choices=task_list.get_tracks(), help='the track this task belongs to')
     parser_comp.add_argument('-u','--uncomplete', dest='is_incomplete', help='mark a task as incomplete') # TODO connect parser
-    parser_del.add_argument('--ignore_collisions', action='store_true', help='ignore task collisions, deleting the first found entry') #TODO
+    parser_comp.add_argument('--ignore_collisions', action='store_true', help='ignore task collisions, deleting the first found entry') #TODO
 
     ### Tracks #TODO connect parser
     parser_tracks = subparsers.add_parser('tracks', help='Manipulate task tracks', aliases=['track','tr'])
     track_subparsers = parser_tracks.add_subparsers(required=True, dest='tracks_action')
 
-    # List tracks #TODO connect parser
+    # List tracks
     parser_list_track = track_subparsers.add_parser('list', help='List tracks', aliases=['ls','l'])
-    parser_list_track.add_argument('-c','--showcolor', action='store_true', dest='color_track_list', help='flag to display colors of tracks.')
+    parser_list_track.add_argument('-n','--nocolor', action='store_false', default=True, dest='color_track_list', help='flag to display colors of tracks.')
 
     # Add track #TODO connect parser
     parser_add_track = track_subparsers.add_parser('add', help='Add new task tracks', aliases=['a'])
@@ -114,7 +115,7 @@ def main():
     # Connect 'list' parser
     if args.subcommand.find('l') == 0:
         # Print task list
-        print_header()
+        print_header("TASKS")
         task_tracks = agrs.list_tracks if args.list_tracks else task_list.get_tracks()
 
         for track in task_tracks:
@@ -197,6 +198,35 @@ def main():
         else:
             parser.error('complete: missing args: specify exactly one of -n DEL_TASK_NUM or DEL_TASK_TXT')
 
+    # Connect 'tracks' parser
+    elif args.subcommand.find('tr') == 0:
+
+        # List tracks
+        if args.tracks_action.find('l') == 0:
+            # TODO: Verbosity
+            # TODO: #complete count
+
+            print_header("TRACKS")
+            tracks_list = task_list.get_tracks()
+
+            if args.color_track_list:
+                tracks = [task_list.get_track_color(track) + track + color.END for track in tracks_list]
+            else:
+                tracks = tracks_list
+
+            row = "\t{}\t\t{}\n"
+            print(row.format(color.UNDERLINE + "NAME" + color.END,color.UNDERLINE+"TOTAL #"+ color.END))
+            for i,track in enumerate(tracks):
+                print(row.format(track,len(task_list.get_tasks_in_track(tracks_list[i]))))
+
+            print("")
+
+        # Add tracks
+        elif args.tracks_action.find('a') == 0:
+            pass
+        # Del tracks
+        elif args.tracks_action.find('d') == 0:
+            pass
 
     task_list.write_to_file(data_file)
 
